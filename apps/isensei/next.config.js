9c6aef417a2ae78e04d17083ada51ad71fa1d821
Progexplorer <progexplorer@gmail.com>
2021-12-20 22:37:43 +0100

    feat: transpile modules in next js

--- apps/isensei/next.config.js
+++ apps/isensei/next.config.js
@@ -1,3 +1,6 @@
+const bsconfig = require('../../bsconfig.json')
+const withPlugins = require('next-compose-plugins')
+
 if (!process.env.NEXTAUTH_URL) {
   console.warn('\x1b[33mwarn', '\x1b[0m', 'NEXTAUTH_URL environment variable is not set.')
   if (process.env.URL) {
@@ -14,45 +17,48 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const nextTranslate = require('next-translate')
-
-module.exports = nextTranslate(
-  withBundleAnalyzer({
-    reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
-    },
-    webpack: (config, { dev, isServer }) => {
-      config.module.rules.push({
-        test: /\.(png|jpe?g|gif|mp4)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]',
-            },
+const config = {
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  eslint: {
+    dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]',
           },
-        ],
-      })
+        },
+      ],
+    })
 
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
 
-      if (!dev && !isServer) {
-        // Replace React with Preact only in client production build
-        // Object.assign(config.resolve.alias, {
-        //   react: 'preact/compat',
-        //   'react/jsx-runtime': require.resolve('react/jsx-runtime'),
-        //   'react-dom/test-utils': 'preact/test-utils',
-        //   'react-dom': 'preact/compat',
-        // })
-      }
+    if (!dev && !isServer) {
+      // Replace React with Preact only in client production build
+      // Object.assign(config.resolve.alias, {
+      //   react: 'preact/compat',
+      //   'react/jsx-runtime': require.resolve('react/jsx-runtime'),
+      //   'react-dom/test-utils': 'preact/test-utils',
+      //   'react-dom': 'preact/compat',
+      // })
+    }
 
-      return config
-    },
-  })
-)
+    return config
+  },
+}
+const withNextTranslate = require('next-translate')(withBundleAnalyzer(config))
+
+const transpileModules = ['rescript'].concat(bsconfig['bs-dependencies'])
+
+const withTM = require('next-transpile-modules')(transpileModules)
+
+module.exports = withPlugins([withTM, withNextTranslate], config)
