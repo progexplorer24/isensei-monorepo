1fde9ad6e0e0ea864670e699461154ad4cb628bf
Progexplorer <progexplorer@gmail.com>
2021-12-24 13:43:02 +0100

    fix: resolution errors

--- apps/isensei/next.config.js
+++ apps/isensei/next.config.js
@@ -1,46 +1,60 @@
-const bsconfig = require('../../bsconfig.json')
-const withPlugins = require('next-compose-plugins')
+const bsconfig = require("../../bsconfig.json");
+const withPlugins = require("next-compose-plugins");
 
 if (!process.env.NEXTAUTH_URL) {
-  console.warn('\x1b[33mwarn', '\x1b[0m', 'NEXTAUTH_URL environment variable is not set.')
+  console.warn(
+    "\x1b[33mwarn",
+    "\x1b[0m",
+    "NEXTAUTH_URL environment variable is not set."
+  );
   if (process.env.URL) {
-    process.env.NEXTAUTH_URL = process.env.URL
+    process.env.NEXTAUTH_URL = process.env.URL;
     console.warn(
-      '\x1b[33mwarn',
-      '\x1b[0m',
+      "\x1b[33mwarn",
+      "\x1b[0m",
       `NEXTAUTH_URL environment variable is not set. Using Netlify URL ${process.env.URL}.`
-    )
+    );
   }
 }
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const config = {
   reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "mjs"],
   eslint: {
-    dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
+    dirs: ["pages", "components", "lib", "layouts", "scripts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
   },
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
       test: /\.(png|jpe?g|gif|mp4)$/i,
       use: [
         {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
+            publicPath: "/_next",
+            name: "static/media/[name].[hash].[ext]",
           },
         },
       ],
-    })
+    });
 
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
+      use: ["@svgr/webpack"],
+    });
 
     if (!dev && !isServer) {
       // Replace React with Preact only in client production build
@@ -52,13 +66,13 @@ const config = {
       // })
     }
 
-    return config
+    return config;
   },
-}
-const withNextTranslate = require('next-translate')(withBundleAnalyzer(config))
+};
+const withNextTranslate = require("next-translate")(withBundleAnalyzer(config));
 
-const transpileModules = ['rescript', 'ui'].concat(bsconfig['bs-dependencies'])
+const transpileModules = ["rescript", "ui"].concat(bsconfig["bs-dependencies"]);
 
-const withTM = require('next-transpile-modules')(transpileModules)
+const withTM = require("next-transpile-modules")(transpileModules);
 
-module.exports = withPlugins([withTM, withNextTranslate], config)
+module.exports = withPlugins([withTM, withNextTranslate], config);
