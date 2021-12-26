0ed1be78553cf17bdd31edfed45780e69f8bd0a4
Progexplorer <progexplorer@gmail.com>
2021-12-26 12:32:48 +0100

    style(isensei): fix prettier styling errors

--- apps/isensei/prisma/index.js
+++ apps/isensei/prisma/index.js
@@ -1,9 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ['query'],
-  })
+    log: ["query"],
+  });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
