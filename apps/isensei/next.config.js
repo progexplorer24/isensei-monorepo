96fd00cd6f223f137e2a380cee7987ea832c503f
Progexplorer <progexplorer@gmail.com>
2021-12-28 13:39:13 +0100

    fix(isensei): content Security Policy errors

--- apps/isensei/next.config.js
+++ apps/isensei/next.config.js
@@ -20,12 +20,13 @@ if (!process.env.NEXTAUTH_URL) {
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self' fonts.gstatic.com cdn.jsdelivr.net;
+  frame-src giscus.app
 `;
 
 const securityHeaders = [
