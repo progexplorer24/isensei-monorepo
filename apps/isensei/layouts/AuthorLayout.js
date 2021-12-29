301d1222b0a69c6cccce0b8b700b19250fd8bec9
Progexplorer <progexplorer@gmail.com>
2021-12-29 12:19:33 +0100

    refactor(isensei): change page title

--- apps/isensei/layouts/AuthorLayout.js
+++ apps/isensei/layouts/AuthorLayout.js
@@ -31,7 +31,7 @@ export default function AuthorLayout({
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {t("headerNavLinks:about")}
+            {t("common:about")}
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
