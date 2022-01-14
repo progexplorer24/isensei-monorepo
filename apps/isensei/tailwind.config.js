0eff2b824b4eb92a1b5a4581f7d32f9fe9423ccc
Progexplorer <progexplorer@gmail.com>
2022-01-14 12:27:34 +0100

    fix(isensei): th dark & scroll button opacity

--- apps/isensei/tailwind.config.js
+++ apps/isensei/tailwind.config.js
@@ -144,7 +144,9 @@ module.exports = {
             },
             strong: { color: theme("colors.gray.100") },
             thead: {
-              color: theme("colors.gray.100"),
+              th: {
+                color: theme("colors.gray.100"),
+              },
             },
             tbody: {
               tr: {
