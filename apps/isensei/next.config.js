076ba5481391179c54597692de38e94203b62fb5
Progexplorer <progexplorer@gmail.com>
2022-04-16 08:20:01 +0200

    refactor(blog): remove file-loader from config

--- apps/isensei/next.config.js
+++ apps/isensei/next.config.js
@@ -111,19 +111,6 @@ const config = {
   },
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            publicPath: "/_next",
-            name: "static/media/[name].[hash].[ext]",
-          },
-        },
-      ],
-    });
-
-    config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
