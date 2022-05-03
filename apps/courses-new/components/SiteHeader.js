d34353ddaba686ffa28716e9a29955d495182c11
Progexplorer <progexplorer@gmail.com>
2022-05-03 10:48:04 +0200

    fix(courses): change nav links

--- apps/courses-new/components/SiteHeader.js
+++ apps/courses-new/components/SiteHeader.js
@@ -63,12 +63,12 @@ const SiteHeader = ({ className }) => {
           <div className="px-2 pt-2 pb-5 border-b border-gray-800 sm:flex sm:border-b-0 sm:py-0 sm:px-0">
             <Link href="/">
               <a className="block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800 sm:text-sm sm:px-2 xl:text-gray-900 xl:hover:bg-gray-200">
-                List your property
+                My Learning
               </a>
             </Link>
             <Link href="/">
               <a className="mt-1 block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800 sm:mt-0 sm:text-sm sm:px-2 sm:ml-2 xl:text-gray-900 xl:hover:bg-gray-200">
-                Trips
+                Notifications
               </a>
             </Link>
             <Link href="/">
