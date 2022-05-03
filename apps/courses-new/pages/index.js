de302cbede061443b0627d2a1a80cae01baaeea7
Progexplorer <progexplorer@gmail.com>
2022-05-03 10:10:28 +0200

    fix(courses): courses cleanup

--- apps/courses-new/pages/index.js
+++ apps/courses-new/pages/index.js
@@ -2,7 +2,7 @@ import SiteHeader from "@/components/SiteHeader";
 import SearchFilters from "@/components/SearchFilters";
 import PropertyCard from "@/components/PropertyCard";
 
-const locations = [
+const topics = [
   {
     title: "Web Development",
     description: "Build Internet of the Future!",
@@ -96,48 +96,48 @@ const locations = [
       },
     ],
   },
-  {
-    title: "Data Science",
-    description: "It's about time to join A.I. era!",
-    properties: [
-      {
-        imageUrl: "/static/images/phoenix-1.jpg",
-        sections: 3,
-        lectures: 2,
-        title: "Modern home in city center",
-        price: 190000,
-        reviewCount: 34,
-        rating: 4,
-      },
-      {
-        imageUrl: "/static/images/phoenix-2.jpg",
-        sections: 4,
-        lectures: 1,
-        title: "Quiet living outside the city",
-        price: 175000,
-        reviewCount: 12,
-        rating: 3,
-      },
-      {
-        imageUrl: "/static/images/phoenix-3.jpg",
-        sections: 5,
-        lectures: 4,
-        title: "Renovated apartment uptown",
-        price: 200000,
-        reviewCount: 54,
-        rating: 5,
-      },
-      {
-        imageUrl: "/static/images/phoenix-4.jpg",
-        sections: 3,
-        lectures: 2,
-        title: "Family home in the suburbs",
-        price: 115000,
-        reviewCount: 34,
-        rating: 4,
-      },
-    ],
-  },
+  // {
+  //   title: "Data Science",
+  //   description: "It's about time to join A.I. era!",
+  //   properties: [
+  //     {
+  //       imageUrl: "/static/images/phoenix-1.jpg",
+  //       sections: 3,
+  //       lectures: 2,
+  //       title: "Modern home in city center",
+  //       price: 190000,
+  //       reviewCount: 34,
+  //       rating: 4,
+  //     },
+  //     {
+  //       imageUrl: "/static/images/phoenix-2.jpg",
+  //       sections: 4,
+  //       lectures: 1,
+  //       title: "Quiet living outside the city",
+  //       price: 175000,
+  //       reviewCount: 12,
+  //       rating: 3,
+  //     },
+  //     {
+  //       imageUrl: "/static/images/phoenix-3.jpg",
+  //       sections: 5,
+  //       lectures: 4,
+  //       title: "Renovated apartment uptown",
+  //       price: 200000,
+  //       reviewCount: 54,
+  //       rating: 5,
+  //     },
+  //     {
+  //       imageUrl: "/static/images/phoenix-4.jpg",
+  //       sections: 3,
+  //       lectures: 2,
+  //       title: "Family home in the suburbs",
+  //       price: 115000,
+  //       reviewCount: 34,
+  //       rating: 4,
+  //     },
+  //   ],
+  // },
   {
     title: "Crypto",
     description: "Learn and understand the next monetary revolution!",
@@ -288,17 +288,17 @@ const locations = [
 ];
 
 export default function Home() {
-  const renderLocations = (locations) => {
-    return locations.map((location, i) => {
+  const renderLocations = (topics) => {
+    return topics.map((topic, i) => {
       return (
         <div key={i} className={i > 0 ? "mt-6" : ""}>
           <div className="px-4 xl:px-8">
-            <h3 className="text-gray-900 text-xl">{location.title}</h3>
-            <p className="text-gray-600">{location.description}</p>
+            <h3 className="text-gray-900 text-xl">{topic.title}</h3>
+            <p className="text-gray-600">{topic.description}</p>
           </div>
           <div className="mt-6 sm:overflow-x-auto sm:overflow-y-hidden">
             <div className="px-4 sm:inline-flex sm:pt-2 sm:pb-8 xl:px-8">
-              {location.properties.map((property, i) => {
+              {topic.properties.map((property, i) => {
                 return (
                   <div
                     key={i}
@@ -326,7 +326,7 @@ export default function Home() {
       <div className="xl:flex-1 xl:flex xl:overflow-y-hidden">
         <SearchFilters />
         <main className="py-6 xl:flex-1 xl:overflow-x-hidden bg-gray-50">
-          {renderLocations(locations)}
+          {renderLocations(topics)}
         </main>
       </div>
     </div>
