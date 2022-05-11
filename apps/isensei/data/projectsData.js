a01ffc0ece08ba877977ee73d175227ba06054ef
Progexplorer <progexplorer@gmail.com>
2022-05-11 14:14:58 +0200

    feat(blog): add projects examples

--- apps/isensei/data/projectsData.js
+++ apps/isensei/data/projectsData.js
@@ -7,10 +7,42 @@ const projectsData = {
       href: "https://github.com/progexplorer24/isensei-monorepo/tree/main/packages",
     },
     {
-      title: "Bindings for Cypress Testing Framework",
+      title: "Cypress Framework Bindings",
       description: `API for writing tests in Rescript using Cypress Testing Framework.`,
       imgSrc: "/static/images/testing.jpeg",
       href: "https://github.com/progexplorer24/rescript-monorepo/blob/master/src/bindings/Cypress.res",
+    },
+    {
+      title: "Unit & Integration Tests",
+      description:
+        "Test examples written in Rescript. They present how you should use Cypress Bindings in Rescript.",
+      imgSrc: "/static/images/testing.jpeg",
+      href: "https://github.com/progexplorer24/rescript-monorepo/tree/master/cypress/integration",
+    },
+    {
+      title: "iSensei Courses Platform",
+      description: "Fully Responsive Online Learning Platform",
+      imgSrc: "/static/images/platform.jpg",
+      href: "https://courses.isensei.pl/",
+    },
+    {
+      title: "iSensei Mockup",
+      description: "Design files created in Figma for iSensei Platform",
+      imgSrc: "/static/images/platform.jpg",
+      href: "https://www.figma.com/file/Hnasdc7eTUNsx3SjC7C3uj/Mockup?node-id=0%3A1",
+    },
+    {
+      title: "Happy Place (Python)",
+      description:
+        "User Authentication built completely from scratch. Project hashes and salts passwords using bcrypt.",
+      imgSrc: "/static/images/happy-place.png",
+      href: "https://replit.com/@PatrykSzymkowia/KindheartedLowFilename",
+    },
+    {
+      title: "Netflix Content EDA (Python)",
+      description: "Exploratory Data Analysis of Netflix content",
+      imgSrc: "/static/images/netflix.png",
+      href: "https://colab.research.google.com/drive/1Fe8i7mf1lpRwmGHmYslApXHL2CrmDqWv?usp=sharing",
     },
   ],
   pl: [
@@ -25,6 +57,39 @@ const projectsData = {
       description: `API umożliwiające pisanie testów jednostkowych, integracyjnych oraz end-to-end w języku Rescript.`,
       imgSrc: "/static/images/testing.jpeg",
       href: "https://github.com/progexplorer24/rescript-monorepo/blob/master/src/bindings/Cypress.res",
+    },
+    {
+      title: "Testy jednostkowe i integracyjne",
+      description:
+        "Setki tesów integracyjnych i jednostkowych, prezentujących działanie wiązań napisanych w Rescript'ie",
+      imgSrc: "/static/images/testing.jpeg",
+      href: "https://github.com/progexplorer24/rescript-monorepo/tree/master/cypress/integration",
+    },
+    {
+      title: "Platforma iSensei",
+      description: "W pełni responsywna strona z kursami",
+      imgSrc: "/static/images/platform.jpg",
+      href: "https://courses.isensei.pl/",
+    },
+    {
+      title: "Mockup iSensei",
+      description: "Mockup platformy z kursami stworzony w Figmie",
+      imgSrc: "/static/images/platform.jpg",
+      href: "https://www.figma.com/file/Hnasdc7eTUNsx3SjC7C3uj/Mockup?node-id=0%3A1",
+    },
+    {
+      title: "Happy Place (Python)",
+      description:
+        "Implementacja systemu uwierzytelniania użytkownika na stronie zbudowana od zera w Python'ie. Hasła są hashowane i solone przy użyciu bcrypt'a",
+      imgSrc: "/static/images/happy-place.png",
+      href: "https://replit.com/@PatrykSzymkowia/KindheartedLowFilename",
+    },
+    {
+      title: "Analiza treści Netflix'a (Python)",
+      description:
+        "Analiza eksploracyjna treści publikowanych na platformie Netflix",
+      imgSrc: "/static/images/netflix.png",
+      href: "https://colab.research.google.com/drive/1Fe8i7mf1lpRwmGHmYslApXHL2CrmDqWv?usp=sharing",
     },
   ],
 };
