5780a5368becf49d5a713ecc3f77980621b050fa
Progexplorer <progexplorer@gmail.com>
2022-05-11 11:20:45 +0200

    feat(mono): remove template app

--- apps/next-isensei-app/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "@/css/globals.css";
-import "@/css/fonts.css";
-import "@/css/tailwind.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
