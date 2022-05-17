d16a8782dd63779bf0725f81252954c549101523
Progexplorer <progexplorer@gmail.com>
2022-05-17 17:15:32 +0200

    fix(blog): border shade

--- apps/isensei/pages/privacy-policy.js
+++ apps/isensei/pages/privacy-policy.js
@@ -17,7 +17,7 @@ export default function Projects({ locale, availableLocales }) {
         availableLocales={availableLocales}
       />
       <div>
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5 border-b border-gray-200 dark:border-gray-800">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5 border-b border-gray-200 dark:border-gray-700">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {t("privacyPolicy:title")}
           </h1>
