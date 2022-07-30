6ef4638e51b6467e9ef4738bb283ae93ae227f2e
iSensei <porgexplorer@gmail.com>
2022-07-30 21:11:56 +0200

    refactor(blog): remove smooth scroll polyfill

--- apps/isensei/pages/_document.js
+++ apps/isensei/pages/_document.js
@@ -28,7 +28,16 @@ class MyDocument extends Document {
             color="#5bbad5"
           />
           <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" content="#000000" />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: light)"
+            content="#fff"
+          />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: dark)"
+            content="#000"
+          />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link
             rel="preload"
