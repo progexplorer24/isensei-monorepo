75c8d8bcc5704aada0713ed50b491d8f0290a9bf
iSensei <porgexplorer@gmail.com>
2022-07-31 22:02:27 +0200

    refactor(tailwind-config): create shared tailwindcss config

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    // app content
+    `src/**/*.{js,ts,jsx,tsx}`,
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {},
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
