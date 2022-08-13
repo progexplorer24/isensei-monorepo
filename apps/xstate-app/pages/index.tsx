b8a485ee8a3ad392e3140b2318c50e35bef18d94
iSensei <porgexplorer@gmail.com>
2022-08-13 14:01:52 +0200

    feat(xstate-app): add todo machine

--- apps/xstate-app/pages/index.tsx
+++ apps/xstate-app/pages/index.tsx
@@ -1,25 +1,94 @@
 import type { NextPage } from "next";
 import { useMachine } from "@xstate/react";
-import { myMachine } from "../machines/myFirstMachine";
+import { machine } from "../machines/todoAppMachine";
+
+const todos = new Set<string>(["Takes bins out", "Do laundry"]);
+
 const Home: NextPage = () => {
-  const [state, send] = useMachine(myMachine);
+  const [state, send] = useMachine(machine, {
+    services: {
+      loadTodos: async () => {
+        return Array.from(todos);
+      },
+      saveTodo: async (context, event) => {
+        todos.add(context.createNewTodoFormInput);
+      },
+      deleteTodo: async (context, event) => {
+        throw new Error("Oh no!");
+        todos.delete(event.todo);
+      },
+    },
+  });
   return (
     <div>
-      {JSON.stringify(state.value)}
-      <button
-        onClick={() => {
-          send("MOUSEOVER");
-        }}
-      >
-        Mouse over
-      </button>
-      <button
-        onClick={() => {
-          send("MOUSEOUT");
-        }}
-      >
-        Mouse out
-      </button>
+      <pre>{JSON.stringify(state.value)}</pre>
+      <pre>{JSON.stringify(state.context)}</pre>
+      <div>
+        {state.matches("Todos Loaded") && (
+          <>
+            {state.context.todos.map((todo) => (
+              <div key={todo}>
+                <p>{todo}</p>
+                <button
+                  onClick={() =>
+                    send({
+                      type: "Delete",
+                      todo,
+                    })
+                  }
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </>
+        )}
+        {state.matches("Todos Loaded") && (
+          <button
+            onClick={() =>
+              send({
+                type: "Create new",
+              })
+            }
+          >
+            Create New
+          </button>
+        )}
+        {state.matches("Deleting Todo errored") && (
+          <>
+            <p>Something went wrong: {state.context.errorMessage}</p>
+            <button
+              onClick={() =>
+                send({
+                  type: "Speed up",
+                })
+              }
+            >
+              Go back to list
+            </button>
+          </>
+        )}
+        {state.matches("Create new todo.Showing form input") && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              send({
+                type: "Submit",
+              });
+            }}
+          >
+            <input
+              type="text"
+              onChange={(e) =>
+                send({
+                  type: "Form input change",
+                  value: e.target.value,
+                })
+              }
+            />
+          </form>
+        )}
+      </div>
     </div>
   );
 };
