822107b195f632a6d5d040f3c7e2d9e33ef607a4
iSensei <porgexplorer@gmail.com>
2022-10-09 17:02:59 +0200

    feat(root): remove auth example & solve build errors

--- apps/isensei/pages/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export const middleware = async (request: NextRequest) => {
-  if (request.nextUrl.pathname.startsWith("/dashboard")) {
-    const authCookie = request.cookies.get("sb-access-token");
-    if (!authCookie) return NextResponse.redirect(new URL("/", request.url));
-  }
-};
