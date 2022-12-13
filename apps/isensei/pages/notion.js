324f9978063567c88ad404a39549b573ee357cd1
iSensei <porgexplorer@gmail.com>
2022-12-13 12:49:57 +0100

    chore(root): update deps

---
+++ apps/isensei/pages/notion.js
@@ -0,0 +1,24 @@
+import { Client } from "@notionhq/client";
+// Notion API page example
+const Notion = ({ roadmap }) => {
+  return <pre>{JSON.stringify(roadmap, null, 2)}</pre>;
+};
+
+export const getStaticProps = async () => {
+  const notion = new Client({
+    auth: process.env.NOTION_SECRET,
+  });
+  const data = await notion.blocks.children.list({
+    block_id: process.env.PAGE_ID,
+  });
+
+  const pages = data.results.filter((result) => result.type === "child_page");
+
+  return {
+    props: {
+      roadmap: pages,
+    },
+  };
+};
+
+export default Notion;
