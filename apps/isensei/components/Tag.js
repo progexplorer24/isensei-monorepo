b0f16a19d7309d5eb6012fbbae40fda2d9cc9486
iSensei <porgexplorer@gmail.com>
2022-12-13 12:52:18 +0100

    fix(blog): update links to v13

--- apps/isensei/components/Tag.js
+++ apps/isensei/components/Tag.js
@@ -3,11 +3,13 @@ import kebabCase from "@/lib/utils/kebabCase";
 
 const Tag = ({ text }) => {
   return (
-    <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-sky-700 hover:text-sky-500 dark:text-primary-500 dark:hover:text-primary-400">
-        {text.split(" ").join("-")}
-      </a>
-    </Link>
+    (<Link
+      href={`/tags/${kebabCase(text)}`}
+      className="mr-3 text-sm font-medium uppercase text-sky-700 hover:text-sky-500 dark:text-primary-500 dark:hover:text-primary-400">
+
+      {text.split(" ").join("-")}
+
+    </Link>)
   );
 };
 
